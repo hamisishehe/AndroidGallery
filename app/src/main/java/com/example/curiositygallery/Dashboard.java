980d41e4721362dbac982c62fe89b6e694323eec
RodriGuez <hamisishehe@gmail.com>
2024-02-08 09:42:32 +0300

    project 1

---
+++ app/src/main/java/com/example/curiositygallery/Dashboard.java
@@ -0,0 +1,103 @@
+package com.example.curiositygallery;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.os.Environment.MEDIA_MOUNTED;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Dashboard extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+
+    static int PERMISSION_REQUEST_CODE=100;
+    RecyclerView recyclerView;
+    ArrayList<String> images;
+    GalleryAdapter adapter;
+    GridLayoutManager manager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+        recyclerView=findViewById(R.id.gallery_recycler);
+        images=new ArrayList<>();
+        adapter=new GalleryAdapter(this,images);
+        manager=new GridLayoutManager(this,3);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(manager);
+        checkPermissions();
+
+    }
+
+    private void checkPermissions() {
+        int result= ContextCompat.checkSelfPermission(getApplicationContext(),READ_EXTERNAL_STORAGE);
+        if(result== PackageManager.PERMISSION_GRANTED){
+            loadImages();
+        }else{
+            ActivityCompat.requestPermissions(this,new String[]{READ_EXTERNAL_STORAGE},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0){
+            boolean accepted=grantResults[0]==PackageManager.PERMISSION_GRANTED;
+            if(accepted){
+                loadImages();
+            }else{
+                Toast.makeText(this,"You have dined the permission",Toast.LENGTH_LONG).show();
+            }
+        }else{
+
+        }
+    }
+
+    private void loadImages() {
+        boolean SDCard= Environment.getExternalStorageState().equals(MEDIA_MOUNTED);
+        if(SDCard){
+            final String[] colums={MediaStore.Images.Media.DATA,MediaStore.Images.Media._ID};
+            final String order=MediaStore.Images.Media.DATE_TAKEN+" DESC";
+            Cursor cursor=getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,colums,null,null,order);
+            int count =cursor.getCount();
+            for(int i=0;i<count;i++){
+                cursor.moveToPosition(i);
+                int colunmindex=cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                images.add(cursor.getString(colunmindex));
+            }
+            recyclerView.getAdapter().notifyDataSetChanged();
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/curiositygallery/GalleryAdapter.java
@@ -0,0 +1,84 @@
+package com.example.curiositygallery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
+
+
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<String> images_list;
+
+    public interface OnImageClickListener {
+        void onImageClick(String imageUrl);
+    }
+
+
+    private OnImageClickListener listener;
+
+    public GalleryAdapter(Context context, ArrayList<String> images_list, OnImageClickListener listener) {
+        this.context = context;
+        this.images_list = images_list;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.grid_item,null,true);
+        return new ViewHolder(view);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        File image_file=new File(images_list.get(position));
+        if(image_file.exists()){
+            Glide.with(context).load(image_file).into(holder.image);
+        }
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = holder.getAdapterPosition();
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    Intent intent = new Intent(context, ImageDetail.class);
+                    intent.putExtra("image_path", images_list.get(adapterPosition));
+                    context.startActivity(intent);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return images_list.size();
+    }
+
+    public GalleryAdapter(Context context, ArrayList<String> images_list) {
+        this.context = context;
+        this.images_list = images_list;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView image;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image=itemView.findViewById(R.id.gallery_item);
+        }
+    }
+}
